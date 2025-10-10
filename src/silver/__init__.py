8dbef394d6c597d72c09d808dd160d0cadff8fb6
pedruck <pedrodmreis@gmail.com>
2025-10-10 18:25:33 -0300

    Adiciona as contribuition metrics
    Co-authored-by: guxvr <guhxe32@gmail.com>

--- src/silver/__init__.py
+++ src/silver/__init__.py
@@ -1,9 +1,11 @@
 
 from .contribution_metrics import process_contribution_metrics
+from .member_analytics import process_member_analytics
 
 __all__ = [
 
-    'process_contribution_metrics'
- 
+    'process_contribution_metrics',
+    'process_member_analytics'
+
 
 ]
