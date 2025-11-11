fa66032d46df8376f18fa847819b6d32eef533fd
Pedro Druck Montalvão Reis <pedrodmreis@gmail.com>
2025-11-11 15:46:58 -0300

    ajustes e finalização

--- front-end/src/pages/Commits.tsx
+++ front-end/src/pages/Commits.tsx
@@ -283,7 +283,7 @@ export default function CommitsPage() {
               title={lineToggle ? 'Hide Line Graph' : 'Show Line Graph'}
               className={`px-4 py-2 text-white rounded-lg transition-colors duration-200 ${
                 lineToggle
-                  ? 'border border-green-500 bg-green-600 hover:bg-green-700'
+                  ? 'border border-gray-400 bg-gray-400 hover:bg-gray-500'
                   : 'border border-gray-500 bg-gray-600 hover:bg-gray-700'
               }`}
             >
