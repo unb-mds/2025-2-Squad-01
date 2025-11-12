0f48b83932c3c7e025e5a4a9a300bd5df72de26a
Gustavo <guhxe32@gmail.com>
2025-11-11 23:30:05 -0300

    feat: add repository visualization extraction pipeline

--- src/bronze/__init__.py
+++ src/bronze/__init__.py
@@ -3,10 +3,12 @@ from .repositories import extract_repositories
 from .members import extract_members
 from .issues import extract_issues
 from .commits import extract_commits
+from .repository_structure import extract_repository_structure
 
 __all__ = [
     'extract_repositories',
     'extract_members', 
     'extract_issues',
-    'extract_commits'
+    'extract_commits',
+    'extract_repository_structure'
 ]
